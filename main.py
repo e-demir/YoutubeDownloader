786f1c3b854fa28b9628b981ba187621c068ec5a
Emrullah DEMİR <e177demir@gmail.com>
2021-12-05 18:56:19 +0300

    Youtube video downlader with Flask
    Project completed

--- main.py
+++ main.py
@@ -1,21 +1,38 @@
-from pytube import YouTube, Playlist
+from pytube import YouTube
+from flask import Flask, render_template, url_for, request
+import time
+from hurry.filesize import size
 
-# print(yt.views)
-# print(yt.title)
-# print(yt.author)
-# print(yt.length)
-# print(yt.age_restricted)
-# print(yt.publish_date)
-# print(yt.description)
-
-yt = YouTube('https://www.youtube.com/watch?v=BqhWfXh_Opg&list=WL&index=2')
-# videos = yt.streams.filter(progressive=True)
-# for video in videos:
-#     print(video.resolution + "indiriliyor...")
-#     video.download(filename=yt.title + "_" + video.resolution)
+app = Flask(__name__)
 
 
-lst = Playlist("https://www.youtube.com/playlist?list=PLzMcBGfZo4-mYPQyPUciU_BkZHYXM42t6")
-# for video in lst.videos:
-#     print("indiriliyor" + video.title)
-#     video.streams.first().download("twt")
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/api/youtube")
+def youtube():
+    url = request.args.get("url")
+    if url:
+        yt = YouTube(url)
+        video = {
+            "info": {
+                "title": yt.title,
+                "author": yt.author,
+                "thumbnail": yt.thumbnail_url,
+                "desc": yt.description,
+                "time": time.strftime("%H:%M:%S", time.gmtime(yt.length)),
+                "views": yt.views,
+                "publish_date": yt.publish_date
+            },
+            "sources": []
+        }
+        videos = yt.streams.filter(progressive=True)
+        for v in videos:
+            video["sources"].append({
+                "url": v.url,
+                "quality": v.resolution,
+                "size": size(v.filesize)
+            })
+        return video
